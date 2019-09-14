4a2b52064c66f03e94642a1245822004b6f1d35f
pixelHat <alisson42@protonmail.com>
2019-09-14 20:17:14 -0300

    Add parallax
    add the parallax effect on play state.

--- main.lua
+++ main.lua
@@ -1,12 +1,13 @@
 require 'src/Dependencies'
 
 function love.load()
+  love.graphics.setDefaultFilter('nearest', 'nearest')
   push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         vsync = true,
         fullscreen = false,
         resizable = true
   })
-  
+
   gStateMachine = StateMachine {
     ['play'] = function() return PlayState() end
   }
@@ -17,8 +18,8 @@ function love.resize(w, h)
   push:resize(w, h)
 end
 
-function love.update()
-
+function love.update(dt)
+  gStateMachine:update(dt)
 end
 
 function love.draw()
--- src/states/PlayState.lua
+++ src/states/PlayState.lua
@@ -1,9 +1,25 @@
 local background = love.graphics.newImage('graphics/terrain/city_background_night.png')
+local ground = love.graphics.newImage('graphics/terrain/ground.png')
 
 PlayState = Class{__includes = BaseState}
 
-function PlayState:enter(enterParams) end
+function PlayState:enter(enterParams)
+  self.Player = Player(100, 100)
+  self.backgroundScroll = 0
+  self.backgroundScrollVelocity = 3
+  self.backgroundWidth = 8192
+  self.groundScroll = 0
+  self.groundScrollVelocity = 500
+  self.groundWidth = 988
+end
+
+function PlayState:update(dt)
+  self.backgroundScroll = (self.backgroundScroll + self.backgroundScrollVelocity*dt) % self.backgroundWidth
+  self.groundScroll = (self.groundScroll + self.groundScrollVelocity*dt) % self.groundWidth
+end
 
 function PlayState:render()
-  love.graphics.draw(background, 0, -100, 0, 0.5)
+  love.graphics.draw(background, -self.backgroundScroll, -100, 0, 0.5)
+  love.graphics.draw(ground, -self.groundScroll, VIRTUAL_HEIGHT - 40, 0, 2)
+  -- self.Player:render()
 end
